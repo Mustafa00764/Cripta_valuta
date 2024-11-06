c44e8d14708f9049c29aab8369cedd619875e9b1
Mustafa00764 <eshchanov9@gmail.com>
2024-11-06 16:33:47 +0500

    user

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -97,7 +97,7 @@ const Header = () => {
           username: user.username,
           first_name: user.first_name,
           last_name: user.last_name,
-          photo_url: "https://cripta-valuta.vercel.app/assets/img1-Br6Mmb9t.svg",
+          photo_url: user.photo_url,
         },
       })
       .then((response) => {
