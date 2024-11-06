80ed3f384f4534cc7821576256b82746c71e6385
Mustafa00764 <eshchanov9@gmail.com>
2024-11-06 19:00:18 +0500

    user

--- src/components/telegramCallback.js
+++ src/components/telegramCallback.js
@@ -2,14 +2,21 @@ import axios from 'axios';
 
 export default async function handler(req, res) {
   const { id, username, first_name, last_name, photo_url } = req.query;
+  
   try {
-    // Перенаправление запроса на ваш внутренний сервер
+    // Отправляем запрос на внутренний сервер, передавая параметры пользователя
     const response = await axios.get(`http://154.53.45.100:8080/auth/telegram/callback`, {
       params: { id, username, first_name, last_name, photo_url }
     });
 
-    // Отправляем результат обратно пользователю
-    res.status(200).json(response.data);
+    // Проверяем, что ответ содержит данные пользователя
+    if (response.data && response.data.user) {
+      // Возвращаем клиенту данные пользователя, полученные с бэкенда
+      res.status(200).json(response.data.user);
+    } else {
+      res.status(404).json({ success: false, message: 'Пользователь не найден' });
+    }
+
   } catch (error) {
     console.error('Ошибка при пересылке запроса:', error);
     res.status(500).json({ success: false, message: 'Ошибка при пересылке запроса' });
