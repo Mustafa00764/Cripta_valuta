66fea47d30dc6c8e4556441311c7f1ab446332c1
Mustafa00764 <eshchanov9@gmail.com>
2024-11-06 19:50:08 +0500

    user

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -105,12 +105,12 @@ const Header = () => {
       })
       .then((response) => {
         setIsAuthenticated(true);
-        setUser(response.data.user)
-        console.log('Успешная аутентификация:', response.data.user);
-        localStorage.setItem("user", JSON.stringify(response.data.user))
+        setUser(response.config.params)
+        console.log('Успешная аутентификация:', response.config.params);
+        localStorage.setItem("user", JSON.stringify(response.config.params))
       })
       .catch((error) => {
-        console.error('Ошибка при аутентификации:', response.cdata);
+        console.error('Ошибка при аутентификации:', response.config.params);
       });
   };
   
