cfe49daec2f5e8c8915326a97ba6ef892045941c
Mustafa00764 <eshchanov9@gmail.com>
2024-11-18 19:45:34 +0500

    new

--- src/pages/EditProfilePage.jsx
+++ src/pages/EditProfilePage.jsx
@@ -129,6 +129,7 @@ const EditProfilePage = () => {
       // Отправляем данные на /upload
       const uploadResponse = await api.post("/upload", formData, {
         headers: {
+          "Content-Type": "multipart/form-data",
           Authorization: `Bearer ${accessToken}`,
         },
       });
