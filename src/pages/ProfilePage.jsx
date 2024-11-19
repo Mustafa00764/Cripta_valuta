c18a022ecc8e40b32da875195bc38d5e0cfd4f80
Mustafa00764 <eshchanov9@gmail.com>
2024-11-19 20:38:34 +0500

    log

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -26,7 +26,7 @@ const ProfilePage = () => {
   return ( 
     <div className='text-textMode'>
       <div className='w-full h-[226px]'>
-        <img src={posterPhoto} alt="" className='w-full h-full object-cover object-top'/>
+        <img src={posterPhoto} alt="" className='w-full h-full object-cover object-center'/>
       </div>
       <div className="container">
         <div className='border flex justify-between bg-bgMode -mt-[20px] md:px-5 w-full rounded-[15px] border-[#494E5B] pl-5 pb-5'>
