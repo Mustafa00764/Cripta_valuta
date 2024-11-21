2811443daf017eb23dcd712d0942c8f597776219
Mustafa00764 <eshchanov9@gmail.com>
2024-11-21 21:55:06 +0500

    log

--- src/admin/CModel.jsx
+++ src/admin/CModel.jsx
@@ -155,9 +155,9 @@ const CModel = () => {
           <button type='submit' className="mt-4 px-4 py-2 bg-blue-500 text-white" >
             PUSH
           </button>
-          <button className="mt-4 px-4 py-2 bg-blue-500 text-white" onClick={()=>setModel(false)}>
+          <div className="mt-4 px-4 py-2 bg-blue-500 text-white" onClick={()=>setModel(false)}>
             Cancel
-          </button>
+          </div>
         </div>
         </form>
       </div>
