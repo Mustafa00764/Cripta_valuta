5f53e487598a3e07f8f3e62b086a1fbfcf860707
Mustafa00764 <eshchanov9@gmail.com>
2024-11-24 15:24:09 +0500

    ll

--- src/admin/CModel.jsx
+++ src/admin/CModel.jsx
@@ -151,11 +151,11 @@ const CModel = () => {
               className={`w-full outline-none border min-h-[150px] border-[#262E34] p-[15px] bg-bgMode transition-all text-textMode rounded-[12px]`}
             ></textarea>
           </div>
-          <div className='w-full flex justify-between items-center'>
-          <button type='submit' className="mt-4 px-4 py-2 bg-blue-500 text-white" >
+          <div className='w-full flex gap-6 justify-between items-center'>
+          <button type='submit' className="mt-4 max-w-[250px] w-full text-[24px] h-[50px] rounded-[8px] bg-blue-500 text-white" >
             PUSH
           </button>
-          <div className="mt-4 px-4 py-2 cursor-pointer bg-blue-500 text-white" onClick={()=>setModel(false)}>
+          <div form="" className="mt-4 max-w-[250px] w-full text-[24px] h-[50px] rounded-[8px] bg-blue-500 text-white" onClick={()=>setModel(false)}>
             Cancel
           </div>
         </div>
