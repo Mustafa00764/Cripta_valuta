c6fdbeec555b9972adb8b21d941500969dc72583
Mustafa00764 <eshchanov9@gmail.com>
2024-11-26 21:34:19 +0500

    ll

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -73,7 +73,7 @@ const Header = () => {
           username: user.username,
           first_name: user.first_name,
           last_name: user.last_name,
-          photo_url: user.photo_url,
+          photo_url: user.photo_url?user.photo_url:LC_logo,
         },
       });
 
