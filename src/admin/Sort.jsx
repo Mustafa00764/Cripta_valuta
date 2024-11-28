faf1e6f785d754967a75fab721c29ab2bbcfb86f
Mustafa00764 <eshchanov9@gmail.com>
2024-11-28 13:42:41 +0500

    ll

--- src/admin/Sort.jsx
+++ src/admin/Sort.jsx
@@ -12,7 +12,7 @@ const Sort = () => {
       <div className='flex items-center gap-1'>
       <button
         className={`w-[50px] h-[50px] ${
-          sort === 10 ? ` border-2 border-[#262E34] ${theme?'text-sideBarTextDark':'text-white'} transition-all ${theme?'bg-sideBarTextLight':'bg-[#151B1F]'}  transition-all` : ` border-2 ${theme?'text-sideBarTextDark':'text-white'} transition-all ${theme?'bg-sideBarLight':'bg-[#262E34]'} border-[#262E34] transition-all ${theme?'hover:bg-sideBarLight':'hover:bg-[#151B1F]'} `
+          sort === 10 ? ` border-2 border-[#262E34] ${theme?'text-sideBarTextDark':'text-white'} bg-pageMode  transition-all` : ` border-2 ${theme?'text-sideBarTextDark':'text-white'} transition-all ${theme?'bg-sideBarLight':'bg-[#262E34]'} border-[#262E34] transition-all ${theme?'hover:bg-sideBarLight':'hover:bg-[#151B1F]'} `
         }`}
         onClick={() => setSort(10)}
       >
@@ -20,7 +20,7 @@ const Sort = () => {
       </button>
       <button
         className={`w-[50px] h-[50px] ${
-          sort === 15 ? ` border-2 border-[#262E34] ${theme?'text-sideBarTextDark':'text-white'} transition-all ${theme?'bg-sideBarTextLight':'bg-[#151B1F]'}  transition-all` : ` border-2 ${theme?'text-sideBarTextDark':'text-white'} transition-all ${theme?'bg-sideBarLight':'bg-[#262E34]'} border-[#262E34] transition-all ${theme?'hover:bg-sideBarLight':'hover:bg-[#151B1F]'} `
+          sort === 15 ? ` border-2 border-[#262E34] ${theme?'text-sideBarTextDark':'text-white'} bg-pageMode  transition-all` : ` border-2 ${theme?'text-sideBarTextDark':'text-white'} transition-all ${theme?'bg-sideBarLight':'bg-[#262E34]'} border-[#262E34] transition-all ${theme?'hover:bg-sideBarLight':'hover:bg-[#151B1F]'} `
         }`}
         onClick={() => setSort(15)}
       >
@@ -28,7 +28,7 @@ const Sort = () => {
       </button>
       <button
         className={`w-[50px] h-[50px] ${
-          sort === 20 ? ` border-2 border-[#262E34] ${theme?'text-sideBarTextDark':'text-white'} transition-all ${theme?'bg-sideBarTextLight':'bg-[#151B1F]'}  transition-all` : ` border-2 ${theme?'text-sideBarTextDark':'text-white'} transition-all ${theme?'bg-sideBarLight':'bg-[#262E34]'} border-[#262E34] transition-all ${theme?'hover:bg-sideBarLight':'hover:bg-[#151B1F]'} `
+          sort === 20 ? ` border-2 border-[#262E34] ${theme?'text-sideBarTextDark':'text-white'} bg-pageMode  transition-all` : ` border-2 ${theme?'text-sideBarTextDark':'text-white'} transition-all ${theme?'bg-sideBarLight':'bg-[#262E34]'} border-[#262E34] transition-all ${theme?'hover:bg-sideBarLight':'hover:bg-[#151B1F]'} `
         }`}
         onClick={() => setSort(20)}
       >
@@ -36,7 +36,7 @@ const Sort = () => {
       </button>
       <button
         className={`w-[50px] h-[50px] ${
-          sort === 25 ? ` border-2 border-[#262E34] ${theme?'text-sideBarTextDark':'text-white'} transition-all ${theme?'bg-sideBarTextLight':'bg-[#151B1F]'}  transition-all` : ` border-2 ${theme?'text-sideBarTextDark':'text-white'} transition-all ${theme?'bg-sideBarLight':'bg-[#262E34]'} border-[#262E34] transition-all ${theme?'hover:bg-sideBarLight':'hover:bg-[#151B1F]'} `
+          sort === 25 ? ` border-2 border-[#262E34] ${theme?'text-sideBarTextDark':'text-white'} bg-pageMode transition-all` : ` border-2 ${theme?'text-sideBarTextDark':'text-white'} transition-all ${theme?'bg-sideBarLight':'bg-[#262E34]'} border-[#262E34] transition-all ${theme?'hover:bg-sideBarLight':'hover:bg-[#151B1F]'} `
         }`}
         onClick={() => setSort(25)}
       >
@@ -44,7 +44,7 @@ const Sort = () => {
       </button>
       <button
         className={`w-[50px] h-[50px] ${
-          sort === 50 ? ` border-2 border-[#262E34] ${theme?'text-sideBarTextDark':'text-white'} transition-all ${theme?'bg-sideBarTextLight':'bg-[#151B1F]'}  transition-all` : ` border-2 ${theme?'text-sideBarTextDark':'text-white'} transition-all ${theme?'bg-sideBarLight':'bg-[#262E34]'} border-[#262E34] transition-all ${theme?'hover:bg-sideBarLight':'hover:bg-[#151B1F]'} `
+          sort === 50 ? ` border-2 border-[#262E34] ${theme?'text-sideBarTextDark':'text-white'} bg-pageMode  transition-all` : ` border-2 ${theme?'text-sideBarTextDark':'text-white'} transition-all ${theme?'bg-sideBarLight':'bg-[#262E34]'} border-[#262E34] transition-all ${theme?'hover:bg-sideBarLight':'hover:bg-[#151B1F]'} `
         }`}
         onClick={() => setSort(50)}
       >
