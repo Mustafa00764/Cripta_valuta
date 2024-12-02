e0c9938b537745808a439a30b21ff529a83be325
Mustafa00764 <eshchanov9@gmail.com>
2024-12-02 21:50:38 +0500

    l

--- src/pages/ArticleInfo.jsx
+++ src/pages/ArticleInfo.jsx
@@ -112,16 +112,16 @@ const ArticleInfo = () => {
               className="h-[408px] ms:h-[344px] w-full absolute top-0 left-0 bg-no-repeat bg-cover bg-center"
             >
               <img src={article.poster} alt="" crossOrigin="anonymous" className="w-full h-full object-cover"/>
-              <div className=" backdrop-blur-2xl w-full h-[101%] bg-blurMode"></div>
+              <div className=" backdrop-blur-2xl z-10 w-full h-[101%] bg-blurMode"></div>
             </div>
             <div className="px-[64px] xl:px-[48px] md:px-6 md:pt-4 xl:pt-[66px] flex xm:flex-col-reverse gap-8 pt-[88px] relative">
               <div>
                 <div
-                  className="w-[629px] xm:w-full sm:h-[312px] relative ms:h-[232px] h-[416px] p-8 ms:p-4 bg-cover bg-center flex items-end"
+                  className="w-[629px] xm:w-full sm:h-[312px] relative ms:h-[232px] h-[416px] ms:p-4 bg-cover bg-center flex items-end"
                 >
-                <img src={article.poster} crossOrigin="anonymous" alt="img" className="w-full h-full absolute object-cover"/>
+                  <img src={article.poster} crossOrigin="anonymous" alt="img" className="w-full h-full absolute object-cover"/>
 
-                  <button className="py-1 px-6 rounded-full bg-[#779CFF] shadow-tagBtn text-[#fff]">
+                  <button className="py-1 px-6 m-8 rounded-full bg-[#779CFF] shadow-tagBtn text-[#fff]">
                     {article.categories}
                   </button> 
 
