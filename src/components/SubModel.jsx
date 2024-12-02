37f0b530b76c18872e70929f5b9041e5375b3fad
Mustafa00764 <eshchanov9@gmail.com>
2024-12-02 22:19:58 +0500

    l

--- src/components/SubModel.jsx
+++ src/components/SubModel.jsx
@@ -4,7 +4,7 @@ import { AuthContext } from '../context/AuthContext'
 const SubModel = () => {
   const {isSubscribed, setIsSubscribed} = useContext(AuthContext)
   return (
-    <div className={isSubscribed?'w-full h-full absolute top-0 left-0 flex justify-center transition-all bg-[rgba(0,0,0,.5)] items-center z-[25] opacity-[1]':'w-full h-full absolute top-0 left-0 flex justify-center transition-all bg-[rgba(0,0,0,.5)] items-center -z-10 opacity-0'}>
+    <div className={!isSubscribed?'w-full h-full absolute top-0 left-0 flex justify-center transition-all bg-[rgba(0,0,0,.5)] items-center z-[25] opacity-[1]':'w-full h-full absolute top-0 left-0 flex justify-center transition-all bg-[rgba(0,0,0,.5)] items-center -z-10 opacity-0'}>
       <div className='max-w-[767px] w-full flex flex-col gap-4 h-auto p-6 bg-bgMode rounded-[15px] '>
         <p className='text-[32px] leading-[48px] font-semibold text-center'>Для продолжения, пожалуйста, подпишитесь на наш Telegram-канал.</p>
         <p className=' text-center'>Подписка обязательна для получения доступа к следующему шагу.</p>
