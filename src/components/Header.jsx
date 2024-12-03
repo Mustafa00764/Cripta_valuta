0c91b2cf294fee79a3235e65749cd9b5b1f80c59
Mustafa00764 <eshchanov9@gmail.com>
2024-12-03 15:45:37 +0500

    l

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -119,7 +119,7 @@ const Header = () => {
       </div>
       <div className=" text-[14px]">
         <div className=" px-6 h-[100%] text-[#fff] flex flex-col pb-1">
-          <NavLink to={`/profile/${user.id}`} onClick={()=>setMenu(false)} className={(navData)=>(navData.isActive?` font-extrabold`:" ")}>
+              <NavLink to={`/profile/${user ? user.id:"55"}`} onClick={()=>setMenu(false)} className={(navData)=>(navData.isActive?` font-extrabold`:" ")}>
           <div className={`w-full hover:font-extrabold transition-all h-[50px] flex gap-[20px] items-center cursor-pointer`}>
               <svg width="28" height="28" strokeWidth="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2Z" stroke="#F4F7FF" strokeLinecap="round" strokeLinejoin="round"/>
