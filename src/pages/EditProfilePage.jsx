96591763cdcbff08baf43cef6f65bd8b9920c130
Mustafa00764 <eshchanov9@gmail.com>
2024-12-04 12:28:08 +0500

    l

--- src/pages/EditProfilePage.jsx
+++ src/pages/EditProfilePage.jsx
@@ -139,7 +139,7 @@ const EditProfilePage = () => {
           Authorization: `Bearer ${newAccessToken}`,
         },
       });
-      console.log(uploadResponse1.data);
+      console.log(uploadResponse1.data.path);
       const image1Path = "https://legitcommunity.uz" + uploadResponse1.data.path;
       if (!image1Path) {
         throw new Error("Ошибка загрузки изображений на сервер.");
