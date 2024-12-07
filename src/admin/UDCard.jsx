270c830c85e09a24aed06d42036263cd060eaaa7
Mustafa00764 <eshchanov9@gmail.com>
2024-12-07 18:15:34 +0500

    l

--- src/admin/UDCard.jsx
+++ src/admin/UDCard.jsx
@@ -94,7 +94,27 @@ const UDCard = ({userInfo,index}) => {
         </svg>
       </div>
       </Link>
+      {
+        userInfo.isBlocked?
         <div className='blocked transition-all' onClick={() => blockModel()}>
+        <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="22"
+        height="22"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <rect x="3" y="11" width="18" height="11" rx="2" ry="2" />
+        <path d="M7 11V7a5 5 0 0 1 9.9-1" stroke='#72787F'/>
+      </svg>
+      </div>
+
+      :        
+      <div className='blocked transition-all' onClick={() => blockModel()}>
         <svg width="22" height="22" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
         <g clipPath="url(#clip0_2214_768)">
         <path d="M12 21C16.9706 21 21 16.9706 21 12C21 7.02944 16.9706 3 12 3C7.02944 3 3 7.02944 3 12C3 16.9706 7.02944 21 12 21Z" stroke="#72787F" strokeWidth="1.5" strokeLinecap="round"/>
@@ -107,6 +127,8 @@ const UDCard = ({userInfo,index}) => {
         </defs>
         </svg>
       </div>
+      }
+
     </div>
   </div>
   )
